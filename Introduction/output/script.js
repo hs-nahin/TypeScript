45edc3b6a02eceead57240b8a50262406f776e06
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-02-04 19:06:00 +0600

    Generics

--- Introduction/output/script.js
+++ Introduction/output/script.js
@@ -21,3 +21,29 @@ drawSomething({
     width: 100,
     height: 100,
 });
+// Generics
+// A generic function that takes an object with properties 'name' and 'age' and returns a new object with an added 'id' property
+const addID = (obj) => {
+    // Generate a random ID
+    let id = Math.floor(Math.random() * 100);
+    // Return a new object that includes all properties of the original object and adds a new 'id' property
+    return Object.assign(Object.assign({}, obj), { id }); // Spread Operator is used to create a new object that includes all properties of the original object and adds a new 'id' property
+};
+// Create a user object and add an ID to it using the generic function
+let user = addID({
+    name: "Nahin",
+    age: 25,
+    city: "Dinajpur", // Additional property 'city' is allowed because the generic type T can have more properties than 'name' and 'age'
+});
+// Log the 'city' property of the user object
+console.log(user.city); // Output: Dinajpur
+// Create an instance of APIResponse with T as object
+const response1 = {
+    status: 200, // HTTP status code indicating success
+    type: "good", // Type of response
+    data: {
+        // Data of type object
+        name: "Mim", // Name property
+        age: 21, // Age property
+    },
+};
--- Introduction/src/script.ts
+++ Introduction/src/script.ts
@@ -35,3 +35,41 @@ drawSomething({
   width: 100,
   height: 100,
 });
+
+// Generics
+// A generic function that takes an object with properties 'name' and 'age' and returns a new object with an added 'id' property
+const addID = <T extends { name: string; age: number }>(obj: T) => {
+  // Generate a random ID
+  let id = Math.floor(Math.random() * 100);
+  // Return a new object that includes all properties of the original object and adds a new 'id' property
+  return { ...obj, id }; // Spread Operator is used to create a new object that includes all properties of the original object and adds a new 'id' property
+};
+
+// Create a user object and add an ID to it using the generic function
+let user = addID({
+  name: "Nahin",
+  age: 25,
+  city: "Dinajpur", // Additional property 'city' is allowed because the generic type T can have more properties than 'name' and 'age'
+});
+
+// Log the 'city' property of the user object
+console.log(user.city); // Output: Dinajpur
+
+// Generics with Interfaces
+// Define a generic interface APIResponse with a type parameter T
+interface APIResponse<T> {
+  status: number; // HTTP status code
+  type: string; // Type of response (e.g., "good", "error")
+  data: T; // Data of type T, which makes this interface flexible
+}
+
+// Create an instance of APIResponse with T as object
+const response1: APIResponse<object> = {
+  status: 200, // HTTP status code indicating success
+  type: "good", // Type of response
+  data: {
+    // Data of type object
+    name: "Mim", // Name property
+    age: 21, // Age property
+  },
+};
