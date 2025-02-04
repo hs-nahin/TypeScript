e15a5ccef6ffbf72ea644997f2bd71c519c5bf95
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2025-02-04 16:57:31 +0600

    class in ts

--- Introduction/output/script.js
+++ Introduction/output/script.js
@@ -1,3 +1,23 @@
 "use strict";
-const countryName = "I Love Bangladesh";
-console.log(countryName);
+const a = ["Nahin"];
+a.push("Mim");
+a.push(1);
+console.log(a);
+// Couple class
+class Couple {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.city = c;
+    }
+    play() {
+        console.log(`${this.name} is playing`);
+    }
+}
+const groom = new Couple("Nahin", 25, "Dhaka");
+const bride = new Couple("Mim", 21, "Dinajpur");
+console.log(groom.name);
+// Array of class
+const couple = [];
+couple.push(groom);
+couple.push(bride);
--- Introduction/src/script.ts
+++ Introduction/src/script.ts
@@ -1,2 +1,31 @@
-const countryName = "I Love Bangladesh";
-console.log(countryName);
+const a: (string | number)[] = ["Nahin"];
+
+a.push("Mim");
+a.push(1);
+console.log(a);
+
+// Couple class
+class Couple {
+  name: string;
+  age: number;
+  city: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.city = c;
+  }
+  play() {
+    console.log(`${this.name} is playing`);
+  }
+}
+
+const groom = new Couple("Nahin", 25, "Dhaka");
+const bride = new Couple("Mim", 21, "Dinajpur");
+
+console.log(groom.name);
+
+// Array of class
+const couple: Couple[] = [];
+couple.push(groom);
+couple.push(bride);
